6a1428592b433fe64763d4a7d94a7ef562963b19
Gopi <58771392+Gopivj@users.noreply.github.com>
2020-01-03 10:06:34 +0530

    Program updated

---
+++ arithmatic.py
@@ -0,0 +1,20 @@
+num1,num2=map(int,input().split())
+operator=input().strip()
+if(operator=='+'):
+  print(a+b)
+elif(operator=='-'):
+  print(a-b)
+elif(operator=='*'):
+  print(a*b)
+elif(operator=='/'):
+  print(a/b)
+elif(operator=='%'):
+  print(a%b)
+elif(operator=='&'):
+  print(a&b)
+elif(operator=='^'):
+  print(a^b)
+elif(operator=='**'):
+  print(a**b)
+elif(operator=='|'):
+  print(a|b)
